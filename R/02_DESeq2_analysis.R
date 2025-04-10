3fac268d53d0276809642cfecfd5e4471750b25d
Natasa Anastasiadou <141711668+natanast@users.noreply.github.com>
2025-04-10 16:04:53 +0300

    Update 02_DESeq2_analysis.R

--- R/02_DESeq2_analysis.R
+++ R/02_DESeq2_analysis.R
@@ -36,7 +36,7 @@ sample_metadata$Group1  = sample_metadata$Group1 |>
 
 
 
-# differential expression analysis 1 ----------
+# differential expression analysis ----------
 
 index = sample_metadata$Sample
 
@@ -65,7 +65,6 @@ fwrite(
 )
 
 
-
 dds = DESeq(dds, test = "Wald")
 
 
@@ -81,78 +80,3 @@ res = res[order(padj)]
 
 
 writexl::write_xlsx(res, paste0("DESeq2_Trisomies_vs_subset_8.xlsx"))
-
-
-
-# differential expression analysis 2 ----------
-
-
-index = sample_metadata$Sample
-
-mm <- df[, index, with = FALSE] |> setDF(rownames = df$gene_name) |> as.matrix()
-
-
-# ~ Group
-state <- list(
-    c("Trisomies", "subset_8"),
-    c("Trisomies", "mutated_no_subset"),
-    c("Trisomies", "subset_4_IgG")
-)
-
-
-for(i in seq_along(state)) {
-    
-    q = state[[i]]
-    
-    
-    s0 = sample_metadata[Group1 %in% c(q[1], q[2])]
-    
-    s0$Group1 <- s0$Group1 |> factor(levels = c(q[2], q[1]))
-    
-    index = s0$Sample
-    
-    mm = df[, index, with = FALSE] |> setDF(rownames = df$gene_name)
-    
-    index2 = mm |> rowSums()
-    
-    mm = mm[which( index2 >= 10 ), ]
-    
-    
-    dds = DESeqDataSetFromMatrix(
-        countData = mm,
-        colData   = s0,
-        design    = ~ Group1
-    )
-    
-    dds               = estimateSizeFactors(dds)
-    normalized_counts = counts(dds, normalized = TRUE)
-    
-    normalized_counts = normalized_counts |> 
-        as.data.frame() |> 
-        setDT(keep.rownames = "GeneID")
-    
-    
-    fwrite(
-        normalized_counts, row.names = FALSE, quote = FALSE, sep = "\t",
-        paste0("gene_counts_clean_DESEq2_normalized_", q[1], "_vs_", q[2], ".txt")
-    )
-    
-    
-    dds = DESeq(dds, test = "Wald")
-    
-    
-    # resultsNames(dds)
-    
-    res = dds |> 
-        results(pAdjustMethod = "fdr", name = paste0("Group1_", q[1],"_vs_", q[2])) |>
-        as.data.frame() |>
-        setDT(keep.rownames = "GeneID")
-    
-    
-    res = res[order(padj), ]
-    
-    res$comparison = paste0(q[1], "_vs_", q[2])
-    
-    writexl::write_xlsx(res, paste0("DESeq2_", q[1], "_vs_", q[2], ".xlsx"))
-    
-}
